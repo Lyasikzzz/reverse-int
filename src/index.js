536f1c62b5fefa941ee1ac51a1be355183384359
Lyasikzzz <dyadyapavliklyas@gmail.com>
2023-02-08 00:07:41 +0300

    final project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
-}
+    let numStr = n + '',
+    result = '';
+    if (numStr.slice(0, 1) == "-") {
+        let minus = numStr.slice(1, numStr.length);
+        for (let i = minus.length; i > 0; i--) {
+            result += minus.slice((i-1), i);
+        }
+    } else {
+        for (let i = numStr.length; i > 0; i--) {
+            result += numStr.slice((i-1), i);
+        }
+    }
+    return +result;
+};
